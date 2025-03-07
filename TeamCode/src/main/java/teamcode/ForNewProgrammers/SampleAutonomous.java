d57fd3dc483517c7dfd6672dfe984fedb934dafc
mabm5 <mabm5@k12albemarle.org>
2025-03-07 12:05:21 -0500

    HOWDY HOWDY!

--- TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleAutonomous.java
+++ TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleAutonomous.java
@@ -1,5 +1,17 @@
 package teamcode.ForNewProgrammers;
 
-public class SampleAutonomous {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+//import objects below
+
+//Name your class below
+
+public class SampleAutonomous {
+    //Instanciate Objects below
+
+    //create initialization below
+
+    //create the loop below
 }
--- TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleTeleop.java
+++ TeamCode/src/main/java/teamcode/ForNewProgrammers/SampleTeleop.java
@@ -1,4 +1,17 @@
 package teamcode.ForNewProgrammers;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+//import objects below
+
+//Name your class below
+
 public class SampleTeleop {
+    //Instanciate Objects below
+
+    //create initialization below
+
+    //create the loop below
 }
